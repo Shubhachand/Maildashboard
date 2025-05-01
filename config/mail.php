8985e0a38b4bf917d11d72db8ad3d4171e3632da
Shubhachand <shubhachandpatel2022@gmail.com>
2025-05-01 21:07:18 +0530

    first commit

--- config/mail.php
+++ config/mail.php
@@ -37,20 +37,15 @@ return [
 
     'mailers' => [
 
-        'default' => env('MAIL_MAILER', 'smtp'),
-
-        'mailers' => [
-    
-            'smtp' => [
-                'transport'    => 'smtp',
-                'host'         => env('MAIL_HOST', '127.0.0.1'),
-                'port'         => env('MAIL_PORT', 587),
-                'encryption'   => env('MAIL_ENCRYPTION', 'tls'),
-                'username'     => env('MAIL_USERNAME'),
-                'password'     => env('MAIL_PASSWORD'),
-                'timeout'      => null,
-                'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url(env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
-            ],
+        'smtp' => [
+            'transport'    => 'smtp',
+            'host'         => env('MAIL_HOST', '127.0.0.1'),
+            'port'         => env('MAIL_PORT', 587),
+            'encryption'   => env('MAIL_ENCRYPTION', 'tls'),
+            'username'     => env('MAIL_USERNAME'),
+            'password'     => env('MAIL_PASSWORD'),
+            'timeout'      => null,
+            'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url(env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
         ],
 
         'ses' => [
@@ -102,6 +97,7 @@ return [
         ],
 
     ],
+
 
     /*
     |--------------------------------------------------------------------------
